90792d08b980c47bf90a2b3d6cc9ebf7f8a023c8
Jayson Mulwa <jayson.mulwa@gmail.com>
2023-06-26 13:06:33 +0300

    chore: updates

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./App";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+//import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <StarRating maxRating={5} size={24} color='red' /> */}
-    <StarRating
+    {/* <StarRating
       maxRating={5}
       message={["Poor", "Fair", "Good", "Very Good", "Excellent"]}
       defaultRating={3}
-    />
+    /> */}
   </React.StrictMode>
 );
